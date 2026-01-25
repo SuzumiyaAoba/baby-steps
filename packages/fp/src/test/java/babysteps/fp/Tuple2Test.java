6ac574a974b067e030a9188eda2d44c07fcf351f
suzumiyaaoba <SuzumiyaAoba@gmail.com>
2026-01-25 11:08:58 +0900

    feat(fp): add transformation methods and tests for `Tuple2`
    - Added `swap`, `mapFirst`, `mapSecond`, and `bimap` methods to `Tuple2` for tuple element transformations.
    - Implemented detailed unit tests in `Tuple2Test` to validate the new methods and their behavior.

--- packages/fp/src/test/java/babysteps/fp/Tuple2Test.java
+++ packages/fp/src/test/java/babysteps/fp/Tuple2Test.java
@@ -20,4 +20,56 @@ class Tuple2Test {
     softly.assertThat(result.first()).isEqualTo("left");
     softly.assertThat(result.second()).isEqualTo("right");
   }
+
+  @Test
+  void swap_expectedSwappedTuple() {
+    // Arrange
+    final var sut = Tuple2.of("left", "right");
+
+    // Act
+    final var result = sut.swap();
+
+    // Assert
+    softly.assertThat(result.first()).isEqualTo("right");
+    softly.assertThat(result.second()).isEqualTo("left");
+  }
+
+  @Test
+  void mapFirst_expectedMappedValue() {
+    // Arrange
+    final var sut = Tuple2.of("left", "right");
+
+    // Act
+    final var result = sut.mapFirst(String::length);
+
+    // Assert
+    softly.assertThat(result.first()).isEqualTo(4);
+    softly.assertThat(result.second()).isEqualTo("right");
+  }
+
+  @Test
+  void mapSecond_expectedMappedValue() {
+    // Arrange
+    final var sut = Tuple2.of("left", "right");
+
+    // Act
+    final var result = sut.mapSecond(String::length);
+
+    // Assert
+    softly.assertThat(result.first()).isEqualTo("left");
+    softly.assertThat(result.second()).isEqualTo(5);
+  }
+
+  @Test
+  void bimap_expectedMappedValues() {
+    // Arrange
+    final var sut = Tuple2.of("left", "right");
+
+    // Act
+    final var result = sut.bimap(String::length, String::length);
+
+    // Assert
+    softly.assertThat(result.first()).isEqualTo(4);
+    softly.assertThat(result.second()).isEqualTo(5);
+  }
 }
